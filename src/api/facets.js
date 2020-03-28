ae327a8d27eaa4ce58837f14d03f7a3c3f7be9ac
therightstuff <adam@industrialcuriosity.com>
2020-03-28 22:00:56 +0200

    updated dependencies and switched facets example to the standard express router

--- src/api/facets.js
+++ src/api/facets.js
@@ -1,59 +1,21 @@
-import resource from 'resource-router-middleware';
+import { Router } from 'express';
 import facets from '../models/facets';
 
-export default ({ config, db }) => resource({
+export default ({config, db}) => {
+	let api = Router();
 
-	/** Property name to store preloaded entity on `request`. */
-	id : 'facet',
+	// /api/facets/ returns list of facets
+	api.get('/', (req, res) => {
+		res.json(facets)
+	});
 
-	/** For requests with an `id`, you can auto-load the entity.
-	 *  Errors terminate the request, success sets `req[id] = data`.
-	 */
-	load(req, id, callback) {
-		let facet = facets.find( facet => facet.id===id ),
-			err = facet ? null : 'Not found';
-		callback(err, facet);
-	},
+	// /api/facets/:facetId returns the requrested facet
+	api.get('/:facetId', (req, res) => {
+		res.json({
+			id: req.params.facetId,
+			facet: facets[req.params.facetId]
+		})
+	});
 
-	/** GET / - List all entities */
-	index({ params }, res) {
-		res.json(facets);
-	},
-
-	/** POST / - Create a new entity */
-	create({ body }, res) {
-		body.id = facets.length.toString(36);
-		facets.push(body);
-		res.json(body);
-	},
-
-	/** GET /:id - Return a given entity
-	 *  facet's value is determined by the
-	 *  load() method above
-	 */
-	read({ facet }, res) {
-		res.json(facet);
-	},
-
-	/** PUT /:id - Update a given entity
-	 *  facet's value is determined by the
-	 *  load() method above
-	 */
-	update({ facet, body }, res) {
-		for (let key in body) {
-			if (key!=='id') {
-				facet[key] = body[key];
-			}
-		}
-		res.sendStatus(204);
-	},
-
-	/** DELETE /:id - Delete a given entity
-	 *  facet's value is determined by the
-	 *  load() method above
-	 */
-	delete({ facet }, res) {
-		facets.splice(facets.indexOf(facet), 1);
-		res.sendStatus(204);
-	}
-});
+	return api;
+}
